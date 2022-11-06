a879a78eaecfac63eada6675fa1d34782ab90fd3
Jingcheng Zhang <diogin@gmail.com>
2022-11-06 17:22:43 +0800

    setcap: test if file exists

--- setcap.bash
+++ setcap.bash
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script is Linux only. Use sudo to execute this shell script.
 # You must mount your filesystem with suid options to enable setcap support.
 
-setcap cap_net_bind_service=+ep ./gocmc
-setcap cap_net_bind_service=+ep ./gorox
+if [ -f gocmc ]; then
+	setcap cap_net_bind_service=+ep ./gocmc
+fi
+
+if [ -f gorox ]; then
+	setcap cap_net_bind_service=+ep ./gorox
+fi

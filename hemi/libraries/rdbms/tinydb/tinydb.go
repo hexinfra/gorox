af7742f62677f57834983a5971f31004d2fc77e3
Jingcheng Zhang <diogin@gmail.com>
2022-11-12 21:20:05 +0800

    add fake tinydb

---
+++ hemi/libraries/rdbms/tinydb/tinydb.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2020-2022 Jingcheng Zhang <diogin@gmail.com>.
+// Copyright (c) 2022-2023 HexInfra Co., Ltd.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE.md file.
+
+// TinyDB is a tiny RDB implementation.
+
+package tinydb
+
+import (
+	"sync"
+)
+
+func OpenTable(path string) (*Table, error) {
+	return nil, nil
+}
+
+type Table struct {
+	lock sync.RWMutex
+}
+
+func (t *Table) RLock() {
+}
+func (t *Table) RUnlock() {
+}
+
+func (t *Table) Lock() {
+}
+func (t *Table) Unlock() {
+}
+
+func (t *Table) Get() (*Rows, int) {
+	return nil, -1
+}
+func (t *Table) Set() int {
+	return -1
+}
+func (t *Table) Add() int {
+	return -1
+}
+func (t *Table) Del() int {
+	return -1
+}
+
+func (t *Table) Close() error {
+	return nil
+}
+
+type Rows struct {
+}
---
+++ hemi/libraries/rdbms/tinydb/tinydb_test.go
@@ -0,0 +1,41 @@
+// Copyright (c) 2020-2022 Jingcheng Zhang <diogin@gmail.com>.
+// Copyright (c) 2022-2023 HexInfra Co., Ltd.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE.md file.
+
+// TinyDB tests.
+
+package tinydb
+
+/*
+type User struct {
+	Name string
+	Age int
+}
+
+func TestGet() {
+	t := OpenTable("user.table", "r", User{})
+	user := t.Get("id = 1")
+	println(user.Name, user.Age)
+	t.Close()
+}
+
+func TestSet() {
+	t := OpenTable("user.table", "rw", User{})
+	t.Set("name = 'jack'", "id = 1")
+	t.Close()
+}
+
+func TestAdd() {
+	t := OpenTable("user.table", "w", User{})
+	user := &User{"Jack", 23}
+	t.Add(user)
+	t.Close()
+}
+
+func TestDel() {
+	t := OpenTable("user.table", "rw", User{})
+	t.Del("id = 1")
+	t.Close()
+}
+*/

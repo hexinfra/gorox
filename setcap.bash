b6242234058d3106641ddf5d5a76ea11f6934f38
Jingcheng Zhang <diogin@gmail.com>
2022-12-09 15:04:53 +0800

    minor tuning on setcap

--- setcap.bash
+++ setcap.bash
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # This script is Linux only. Use sudo to execute this shell script.
-# You must mount your filesystem with suid options to enable setcap support.
+# You must mount your filesystem with suid option to enable setcap support.
 
 if [ -f gocmc ]; then
-	setcap cap_net_bind_service=+ep ./gocmc
+	setcap cap_net_bind_service=+ep gocmc
 fi
 
 if [ -f gorox ]; then
-	setcap cap_net_bind_service=+ep ./gorox
+	setcap cap_net_bind_service=+ep gorox
 fi

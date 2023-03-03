9d7a0cfbb549ef6b1571bd625171cb12ab039c1b
Zhang Jingcheng <diogin@gmail.com>
2023-03-03 20:48:07 +0800

    standard is not for now

--- hemi/contrib/import.go
+++ hemi/contrib/import.go
@@ -3,11 +3,11 @@
 // All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE.md file.
 
-// Standard components.
+// Preloaded components.
 
-package standard
+package contrib
 
-import ( // all standard components are preloaded
+import (
 	_ "github.com/hexinfra/gorox/hemi/contrib/cachers/local"
 	_ "github.com/hexinfra/gorox/hemi/contrib/cronjobs/clean"
 	_ "github.com/hexinfra/gorox/hemi/contrib/cronjobs/stat"

b89876c80b9e58241ea378ff5d9e133bc00b5169
Zhang Jingcheng <diogin@gmail.com>
2023-03-22 12:32:55 +0800

    add more logic to hemi/example

--- hemi/example/main.go
+++ hemi/example/main.go
@@ -2,31 +2,17 @@ package main
 
 import (
 	. "github.com/hexinfra/gorox/hemi"
+	"github.com/hexinfra/gorox/hemi/contrib/routers/simple"
 	"os"
 	"path/filepath"
 	"runtime"
 )
 
-var config = `
-stage {
-    app "example" {
-        .hostnames = ("*")
-        .webRoot   = %baseDir
-        rule $path == "/favicon.ico" {
-            favicon {}
-        }
-        rule {
-            static {
-                .autoIndex = true
-            }
-        }
-    }
-    httpxServer "main" {
-        .forApps = ("example")
-        .address = ":3080"
-    }
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
-`
 
 func main() {
 	exePath, err := os.Executable()
@@ -37,11 +23,40 @@ func main() {
 	}
 
 	// Begin
+	RegisterHandlet("myHandlet", func(name string, stage *Stage, app *App) Handlet {
+		h := new(myHandlet)
+		h.onCreate(name, stage, app)
+		return h
+	})
+
 	SetBaseDir(baseDir)
 	SetDataDir(baseDir + "/data")
 	SetLogsDir(baseDir + "/logs")
 	SetTempDir(baseDir + "/temp")
 
+	var config = `
+stage {
+    app "example" {
+        .hostnames = ("*")
+        .webRoot   = %baseDir + "/web"
+        rule $path == "/favicon.ico" {
+            favicon {}
+        }
+        rule $path -f {
+            static {
+                .autoIndex = true
+            }
+        }
+        rule {
+            myHandlet {}
+        }
+    }
+    httpxServer "main" {
+        .forApps = ("example")
+        .address = ":3080"
+    }
+}
+`
 	stage, err := ApplyText(config)
 	must(err)
 	stage.Start(0)
@@ -50,8 +65,39 @@ func main() {
 	select {}
 }
 
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
+type myHandlet struct {
+	Handlet_
+	stage *Stage
+	app   *App
+}
+
+func (h *myHandlet) onCreate(name string, stage *Stage, app *App) {
+	h.CompInit(name)
+	h.stage = stage
+	h.app = app
+	r := simple.New()
+	r.Link("/foo", h.foo)
+	h.SetRouter(h, r)
+}
+func (h *myHandlet) OnShutdown() {
+	h.app.SubDone()
+}
+
+func (h *myHandlet) OnConfigure() {}
+func (h *myHandlet) OnPrepare()   {}
+
+func (h *myHandlet) Handle(req Request, resp Response) (next bool) {
+	h.Dispatch(req, resp, h.notFound)
+	return
+}
+func (h *myHandlet) notFound(req Request, resp Response) {
+	resp.Send("handle not found!")
+}
+
+func (h *myHandlet) GET_(req Request, resp Response) {
+	resp.Send("hello, world!")
+}
+func (h *myHandlet) foo(req Request, resp Response) {
+	resp.Push("foo")
+	resp.Push("bar")
 }

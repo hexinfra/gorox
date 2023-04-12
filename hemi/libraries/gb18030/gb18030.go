01016cc3f2d6238d9e75f621d8fb24ce52234082
Zhang Jingcheng <diogin@gmail.com>
2023-04-12 11:10:11 +0800

    prepare gb18030 encoding

---
+++ hemi/libraries/gb18030/gb18030.go
@@ -0,0 +1,23 @@
+// Copyright (c) 2020-2023 Zhang Jingcheng <diogin@gmail.com>.
+// Copyright (c) 2022-2023 HexInfra Co., Ltd.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE.md file.
+
+// Conversions between UTF-8 and GB18030-2022.
+
+package gb18030
+
+func Len(gb string) int {
+	// TODO
+	return 0
+}
+
+func ToUTF8(gb string) (utf string) {
+	// TODO
+	return
+}
+
+func FromUTF8(utf string) (gb string) {
+	// TODO
+	return
+}

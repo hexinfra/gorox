3c3e780ce78a00768223310ea6fe4cf5a6265a33
Zhang Jingcheng <diogin@gmail.com>
2023-04-14 03:57:27 +0800

    add initial gweb protocol

---
+++ hemi/internal/agent_gweb.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2020-2023 Zhang Jingcheng <diogin@gmail.com>.
+// Copyright (c) 2022-2023 HexInfra Co., Ltd.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE.md file.
+
+// GWEB agent handlet passes requests to backend GWEB servers and cache responses.
+
+package internal
+
+// gwebAgent
+type gwebAgent struct {
+}
+
+// gStream
+type gStream struct {
+}
+
+// gRequest
+type gRequest struct {
+	gwebOut_
+}
+
+// gResponse
+type gResponse struct {
+	gwebIn_
+}
+
+//////////////////////////////////////// GWEB protocol elements.
---
+++ hemi/internal/gweb.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2020-2023 Zhang Jingcheng <diogin@gmail.com>.
+// Copyright (c) 2022-2023 HexInfra Co., Ltd.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE.md file.
+
+// GWEB protocol elements, general incoming message and outgoing message implementation.
+
+package internal
+
+// gwebIn_
+type gwebIn_ struct {
+}
+
+// gwebOut_
+type gwebOut_ struct {
+}

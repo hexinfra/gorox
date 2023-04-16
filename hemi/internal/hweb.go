d654db4922948c04f300f000a9cf5e2f3426b24d
Zhang Jingcheng <diogin@gmail.com>
2023-04-16 22:57:23 +0800

    update hweb protocol

--- hemi/internal/hweb.go
+++ hemi/internal/hweb.go
@@ -7,10 +7,7 @@
 
 // HWEB is a binary HTTP/1.1 protocol. It borrows some ideas from HTTP/2, but is
 // simplified and optimized for IDC's internal communication. Its simple design
-// makes it very simple to implement as a server or client.
-
-// Currently HWEB only supports normal request/response mode. Other HTTP modes,
-// like WebSocket, TCP tunnel, and UDP tunnel are not supported.
+// makes it very simple to implement a server or client.
 
 package internal
 
@@ -95,69 +92,77 @@ func (r *hwebOut_) writeVectorH() error {
 
 // recordHead(64) = type(8) streamID(24) flags(8) bodySize(24)
 
-// prefaceRecord = recordHead *setting
-//   setting(32) = code(8) value(24)
+// initRecord = recordHead *setting
+//   setting(32) = settingCode(8) settingValue(24)
 
-// request  = headersRecord *segmentRecord [ trailersRecord ]
-// response = headersRecord *segmentRecord [ trailersRecord ]
+// sizeRecord = recordHead bufferSize(3)
 
-// headersRecord  = recordHead 1*nameValue
-// trailersRecord = recordHead 1*nameValue
+// request  = headRecord *dataRecord [ tailRecord ]
+// response = headRecord *dataRecord [ tailRecord ]
+
+// headRecord = recordHead 1*nameValue
+// tailRecord = recordHead 1*nameValue
 
 //   nameValue = nameSize(8) valueSize(24) name value
-//     name = 1*OCTET
+//     name  = 1*OCTET
 //     value = *OCTET
 
-// segmentRecord = recordHead *OCTET
+// dataRecord = recordHead *OCTET
 
-// On TCP connection established, client sends a preface record, then server sends one, too.
+// On TCP connection established, client sends an init record, then server sends one, too.
 // Whenever a client kicks a new request, it must use the least unused streamID starting from 1.
 // A streamID is considered as unused after a response with this streamID was received entirely.
-// If concurrent streams exceeds the limit set in preface, server can close the connection.
+// If concurrent streams exceeds the limit set in init record, server can close the connection.
 
 const ( // record types
-	hwebTypePreface  = 0 // contains connection settings
-	hwebTypeHeaders  = 1 // contains name-value pairs for headers
-	hwebTypeSegment  = 2 // contains content segment
-	hwebTypeTrailers = 3 // contains name-value pairs for trailers
+	hwebTypeInit = 0 // contains connection settings
+	hwebTypeSize = 1 // available buffer size for receiving content
+	hwebTypeHead = 2 // contains name-value pairs for headers
+	hwebTypeData = 3 // contains content data
+	hwebTypeTail = 4 // contains name-value pairs for trailers
 )
 
 const ( // setting codes
 	hwebSettingMaxRecordBodySize    = 0
-	hwebSettingMaxStreams           = 1
-	hwebSettingMaxConcurrentStreams = 2
+	hwebSettingInitialBufferSize    = 1
+	hwebSettingMaxStreams           = 2
+	hwebSettingMaxConcurrentStreams = 3
 )
 
 var hwebSettingDefaults = [...]int32{
 	hwebSettingMaxRecordBodySize:    16376, // allow: [16376-16777215]
-	hwebSettingMaxStreams:           1000,  // allow: [1-16777215]
-	hwebSettingMaxConcurrentStreams: 100,   // allow: [1-16777215]. cannot be larger than maxStreams
+	hwebSettingInitialBufferSize:    16376, // allow: [16376-16777215]
+	hwebSettingMaxStreams:           1000,  // allow: [100-16777215]
+	hwebSettingMaxConcurrentStreams: 100,   // allow: [100-16777215]. cannot be larger than maxStreams
 }
 
 /*
 
 on connection established:
 
-    -> type=preface streamID=0 bodySize=4 body=[maxRecordBodySize=16376]
-    <- type=preface streamID=0 bodySize=12 body=[maxRecordBodySize=16376 maxStreams=1000 maxConcurrentStreams=10]
+    ==> type=INIT streamID=0 bodySize=8     body=[maxRecordBodySize=16376 initialBufferSize=16376]
+    <== type=INIT streamID=0 bodySize=16    body=[maxRecordBodySize=16376 initialBufferSize=16376 maxStreams=1000 maxConcurrentStreams=10]
 
 stream=1 (sized output):
 
-    -> type=headers streamID=1 bodySize=? body=[:method=GET :target=/hello host=example.com:8081]
+    --> type=HEAD streamID=1 bodySize=?     body=[:method=GET :target=/hello host=example.com:8081]
 
-    <- type=headers streamID=1 bodySize=? body=[:status=200 content-length=12]
-    <- type=segment streamID=1 bodySize=6 body=[hello,]
-    <- type=segment streamID=1 bodySize=6 body=[world!]
+    <-- type=HEAD streamID=1 bodySize=?     body=[:status=200 content-length=12]
+    <-- type=DATA streamID=1 bodySize=6     body=[hello,]
+    <-- type=DATA streamID=1 bodySize=6     body=[world!]
 
 stream=2 (unsized output):
 
-    -> type=headers streamID=2 bodySize=? body=[:method=POST :target=/abc?d=e host=example.com:8081 content-length=90]
-    -> type=segment streamID=2 bodySize=90 body=[...90...]
+    --> type=HEAD streamID=2 bodySize=?     body=[:method=POST :target=/abc?d=e host=example.com:8081 content-length=90]
+    --> type=DATA streamID=2 bodySize=90    body=[...90...]
 
-    <- type=headers streamID=2 bodySize=? body=[:status=200 content-type=text/html]
-    <- type=segment streamID=2 bodySize=77 body=[...77...]
-    <- type=segment streamID=2 bodySize=88 body=[...88...]
-    <- type=segment streamID=2 bodySize=0 body=[] // ends of content
-    <- type=trailers streamID=2 bodySize=? body=[md5-digest=12345678901234567890123456789012]
+    <-- type=HEAD streamID=2 bodySize=?     body=[:status=200 content-type=text/html;charset=utf-8]
+    <-- type=DATA streamID=2 bodySize=16376 body=[...16376...]
+    ==> type=SIZE streamID=2 bodySize=3     body=123
+    <-- type=DATA streamID=2 bodySize=123   body=[...123...]
+    ==> type=SIZE streamID=2 bodySize=3     body=16376
+    <-- type=DATA streamID=2 bodySize=4567  body=[...4567...]
+    <-- type=DATA streamID=2 bodySize=0     body=[] // end of response content
+    <-- type=TAIL streamID=2 bodySize=?     body=[md5-digest=12345678901234567890123456789012]
 
 */

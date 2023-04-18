hexinfra/gorox

commit 8b04e464ac4a791f14b60998cf2e97a38a049ffc

use Prop() to find property and remove Find()

cmds/goops/srvs/rocks/srv.go
@@ -53,7 +53,7 @@ func (s *RocksServer) OnShutdown() {
 
 func (s *RocksServer) OnConfigure() {
 	// address
-	if v, ok := s.Find("address"); ok {
+	if v, ok := s.Prop("address"); ok {
 		if address, ok := v.String(); ok {
 			if p := strings.IndexByte(address, ':'); p == -1 || p == len(address)-1 {
 				UseExitln("bad address: " + address)

hemi/contrib/handlets/hostname/checker.go
@@ -42,7 +42,7 @@ func (h *hostnameChecker) OnShutdown() {
 
 func (h *hostnameChecker) OnConfigure() {
 	// hostname
-	if v, ok := h.Find("hostname"); ok {
+	if v, ok := h.Prop("hostname"); ok {
 		if hostname, ok := v.String(); ok {
 			h.hostname = hostname
 		} else {

hemi/contrib/handlets/sitex/handlet.go
@@ -47,7 +47,7 @@ func (h *Sitex) OnShutdown() {
 
 func (h *Sitex) OnConfigure() {
 	// sites
-	v, ok := h.Find("sites")
+	v, ok := h.Prop("sites")
 	if !ok {
 		UseExitln("sites must be defined")
 	}

hemi/internal/agent_ajp.go
@@ -54,7 +54,7 @@ func (h *ajpAgent) OnShutdown() {
 func (h *ajpAgent) OnConfigure() {
 	h.contentSaver_.onConfigure(h, TempDir()+"/ajp/"+h.name)
 	// toBackend
-	if v, ok := h.Find("toBackend"); ok {
+	if v, ok := h.Prop("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := h.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)
@@ -70,7 +70,7 @@ func (h *ajpAgent) OnConfigure() {
 		UseExitln("toBackend is required for ajpAgent")
 	}
 	// withCacher
-	if v, ok := h.Find("withCacher"); ok {
+	if v, ok := h.Prop("withCacher"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if cacher := h.stage.Cacher(name); cacher == nil {
 				UseExitf("unknown cacher: '%s'\n", name)

hemi/internal/agent_fcgi.go
@@ -72,7 +72,7 @@ func (h *fcgiAgent) OnShutdown() {
 func (h *fcgiAgent) OnConfigure() {
 	h.contentSaver_.onConfigure(h, TempDir()+"/fcgi/"+h.name)
 	// toBackend
-	if v, ok := h.Find("toBackend"); ok {
+	if v, ok := h.Prop("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := h.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)
@@ -88,7 +88,7 @@ func (h *fcgiAgent) OnConfigure() {
 		UseExitln("toBackend is required for fcgiAgent")
 	}
 	// withCacher
-	if v, ok := h.Find("withCacher"); ok {
+	if v, ok := h.Prop("withCacher"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if cacher := h.stage.Cacher(name); cacher == nil {
 				UseExitf("unknown cacher: '%s'\n", name)

hemi/internal/agent_uwsgi.go
@@ -50,7 +50,7 @@ func (h *uwsgiAgent) OnShutdown() {
 func (h *uwsgiAgent) OnConfigure() {
 	h.contentSaver_.onConfigure(h, TempDir()+"/uwsgi/"+h.name)
 	// toBackend
-	if v, ok := h.Find("toBackend"); ok {
+	if v, ok := h.Prop("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := h.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)
@@ -66,7 +66,7 @@ func (h *uwsgiAgent) OnConfigure() {
 		UseExitln("toBackend is required for uwsgiAgent")
 	}
 	// withCacher
-	if v, ok := h.Find("withCacher"); ok {
+	if v, ok := h.Prop("withCacher"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if cacher := h.stage.Cacher(name); cacher == nil {
 				UseExitf("unknown cacher: '%s'\n", name)

hemi/internal/client.go
@@ -133,7 +133,7 @@ func (b *backend_[N]) onCreate(name string, stage *Stage, creator interface{ cre
 func (b *backend_[N]) onConfigure() {
 	b.client_.onConfigure()
 	// nodes
-	v, ok := b.Find("nodes")
+	v, ok := b.Prop("nodes")
 	if !ok {
 		UseExitln("nodes is required for backends")
 	}

hemi/internal/component.go
@@ -175,7 +175,6 @@ type Component interface {
 	Name() string
 
 	OnConfigure()
-	Find(name string) (value Value, ok bool)
 	Prop(name string) (value Value, ok bool)
 	ConfigureBool(name string, prop *bool, defaultValue bool)
 	ConfigureInt64(name string, prop *int64, check func(value int64) bool, defaultValue int64)
@@ -222,16 +221,12 @@ func (c *Component_) MakeComp(name string) {
 
 func (c *Component_) Name() string { return c.name }
 
-func (c *Component_) Find(name string) (value Value, ok bool) {
+func (c *Component_) Prop(name string) (value Value, ok bool) {
 	for component := c.shell; component != nil; component = component.getParent() {
-		if value, ok = component.Prop(name); ok {
+		if value, ok = c.props[name]; ok {
 			break
 		}
 	}
-	return
-}
-func (c *Component_) Prop(name string) (value Value, ok bool) {
-	value, ok = c.props[name]
 	return
 }
 
@@ -273,7 +268,7 @@ func (c *Component_) ConfigureStringDict(name string, prop *map[string]string, c
 }
 
 func _configureProp[T any](c *Component_, name string, prop *T, conv func(*Value) (T, bool), check func(value T) bool, defaultValue T) {
-	if v, ok := c.Find(name); ok {
+	if v, ok := c.Prop(name); ok {
 		if value, ok := conv(&v); ok && (check == nil || check(value)) {
 			*prop = value
 		} else {

commit 69656309aedff2342e68f88ebd4674b5f990e919

fix bug introduced in 8b04e464ac4a791f14b60998cf2e97a38a049ffc

.gitignore
@@ -22,27 +22,20 @@
 desktop.ini
 Thumbs.db
 
-/dist
+dist/
+logs/
+temp/
+vars/
+
 /gomake
 
-/logs
-/temp
-/vars
 /goben
 /goops
 /gorox
 
-/hemi/develop/logs
-/hemi/develop/temp
-/hemi/develop/vars
 /hemi/develop/develop
 
-/hemi/example/logs
-/hemi/example/temp
-/hemi/example/vars
-/hemi/example/example
+/hemi/examples/embed/embed
+/hemi/examples/myapp/myapp
 
-/hemi/gosites/logs
-/hemi/gosites/temp
-/hemi/gosites/vars
 /hemi/gosites/gosites

cmds/goops/srvs/rocks/srv.go
@@ -53,7 +53,7 @@ func (s *RocksServer) OnShutdown() {
 
 func (s *RocksServer) OnConfigure() {
 	// address
-	if v, ok := s.Prop("address"); ok {
+	if v, ok := s.Find("address"); ok {
 		if address, ok := v.String(); ok {
 			if p := strings.IndexByte(address, ':'); p == -1 || p == len(address)-1 {
 				UseExitln("bad address: " + address)

hemi/contrib/handlets/hostname/checker.go
@@ -42,7 +42,7 @@ func (h *hostnameChecker) OnShutdown() {
 
 func (h *hostnameChecker) OnConfigure() {
 	// hostname
-	if v, ok := h.Prop("hostname"); ok {
+	if v, ok := h.Find("hostname"); ok {
 		if hostname, ok := v.String(); ok {
 			h.hostname = hostname
 		} else {

hemi/contrib/handlets/sitex/handlet.go
@@ -47,7 +47,7 @@ func (h *Sitex) OnShutdown() {
 
 func (h *Sitex) OnConfigure() {
 	// sites
-	v, ok := h.Prop("sites")
+	v, ok := h.Find("sites")
 	if !ok {
 		UseExitln("sites must be defined")
 	}

hemi/internal/agent_ajp.go
@@ -54,7 +54,7 @@ func (h *ajpAgent) OnShutdown() {
 func (h *ajpAgent) OnConfigure() {
 	h.contentSaver_.onConfigure(h, TempDir()+"/ajp/"+h.name)
 	// toBackend
-	if v, ok := h.Prop("toBackend"); ok {
+	if v, ok := h.Find("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := h.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)
@@ -70,7 +70,7 @@ func (h *ajpAgent) OnConfigure() {
 		UseExitln("toBackend is required for ajpAgent")
 	}
 	// withCacher
-	if v, ok := h.Prop("withCacher"); ok {
+	if v, ok := h.Find("withCacher"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if cacher := h.stage.Cacher(name); cacher == nil {
 				UseExitf("unknown cacher: '%s'\n", name)

hemi/internal/agent_fcgi.go
@@ -72,7 +72,7 @@ func (h *fcgiAgent) OnShutdown() {
 func (h *fcgiAgent) OnConfigure() {
 	h.contentSaver_.onConfigure(h, TempDir()+"/fcgi/"+h.name)
 	// toBackend
-	if v, ok := h.Prop("toBackend"); ok {
+	if v, ok := h.Find("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := h.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)
@@ -88,7 +88,7 @@ func (h *fcgiAgent) OnConfigure() {
 		UseExitln("toBackend is required for fcgiAgent")
 	}
 	// withCacher
-	if v, ok := h.Prop("withCacher"); ok {
+	if v, ok := h.Find("withCacher"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if cacher := h.stage.Cacher(name); cacher == nil {
 				UseExitf("unknown cacher: '%s'\n", name)

hemi/internal/agent_uwsgi.go
@@ -50,7 +50,7 @@ func (h *uwsgiAgent) OnShutdown() {
 func (h *uwsgiAgent) OnConfigure() {
 	h.contentSaver_.onConfigure(h, TempDir()+"/uwsgi/"+h.name)
 	// toBackend
-	if v, ok := h.Prop("toBackend"); ok {
+	if v, ok := h.Find("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := h.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)
@@ -66,7 +66,7 @@ func (h *uwsgiAgent) OnConfigure() {
 		UseExitln("toBackend is required for uwsgiAgent")
 	}
 	// withCacher
-	if v, ok := h.Prop("withCacher"); ok {
+	if v, ok := h.Find("withCacher"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if cacher := h.stage.Cacher(name); cacher == nil {
 				UseExitf("unknown cacher: '%s'\n", name)

hemi/internal/client.go
@@ -133,7 +133,7 @@ func (b *backend_[N]) onCreate(name string, stage *Stage, creator interface{ cre
 func (b *backend_[N]) onConfigure() {
 	b.client_.onConfigure()
 	// nodes
-	v, ok := b.Prop("nodes")
+	v, ok := b.Find("nodes")
 	if !ok {
 		UseExitln("nodes is required for backends")
 	}

hemi/internal/component.go
@@ -175,6 +175,7 @@ type Component interface {
 	Name() string
 
 	OnConfigure()
+	Find(name string) (value Value, ok bool)
 	Prop(name string) (value Value, ok bool)
 	ConfigureBool(name string, prop *bool, defaultValue bool)
 	ConfigureInt64(name string, prop *int64, check func(value int64) bool, defaultValue int64)
@@ -221,12 +222,16 @@ func (c *Component_) MakeComp(name string) {
 
 func (c *Component_) Name() string { return c.name }
 
-func (c *Component_) Prop(name string) (value Value, ok bool) {
+func (c *Component_) Find(name string) (value Value, ok bool) {
 	for component := c.shell; component != nil; component = component.getParent() {
-		if value, ok = c.props[name]; ok {
+		if value, ok = component.Prop(name); ok {
 			break
 		}
 	}
+	return
+}
+func (c *Component_) Prop(name string) (value Value, ok bool) {
+	value, ok = c.props[name]
 	return
 }
 
@@ -268,7 +273,7 @@ func (c *Component_) ConfigureStringDict(name string, prop *map[string]string, c
 }
 
 func _configureProp[T any](c *Component_, name string, prop *T, conv func(*Value) (T, bool), check func(value T) bool, defaultValue T) {
-	if v, ok := c.Prop(name); ok {
+	if v, ok := c.Find(name); ok {
 		if value, ok := conv(&v); ok && (check == nil || check(value)) {
 			*prop = value
 		} else {

hemi/internal/config.go
@@ -771,7 +771,7 @@ func (c *config) parseValue(component Component, prop string, value *Value) {
 	case tokenProperty: // .property
 		if propRef := current.text; prop == "" || prop == propRef {
 			panic(errors.New("config error: cannot refer to self"))
-		} else if valueRef, ok := component.Prop(propRef); !ok {
+		} else if valueRef, ok := component.Find(propRef); !ok {
 			panic(fmt.Errorf("config error: refer to a prop that doesn't exist in line %d\n", current.line))
 		} else {
 			*value = valueRef
@@ -802,7 +802,7 @@ func (c *config) parseValue(component Component, prop string, value *Value) {
 		} else if c.currentIs(tokenProperty) {
 			if propRef := current.text; prop == "" || prop == propRef {
 				panic(errors.New("config error: cannot refer to self"))
-			} else if valueRef, ok := component.Prop(propRef); !ok {
+			} else if valueRef, ok := component.Find(propRef); !ok {
 				panic(errors.New("config error: refere to a prop that doesn't exist"))
 			} else {
 				str = valueRef

hemi/internal/mesher.go
@@ -51,7 +51,7 @@ func (m *mesher_[M, G, D, E, C]) shutdownSubs() {
 func (m *mesher_[M, G, D, E, C]) onConfigure() {
 	m.Server_.OnConfigure()
 	// accessLog
-	if v, ok := m.Prop("accessLog"); ok {
+	if v, ok := m.Find("accessLog"); ok {
 		if log, ok := v.StringListN(2); ok {
 			m.accessLog = log
 		} else {

hemi/internal/relay_quic.go
@@ -37,7 +37,7 @@ func (f *quicRelay) OnShutdown() {
 
 func (f *quicRelay) OnConfigure() {
 	// toBackend
-	if v, ok := f.Prop("toBackend"); ok {
+	if v, ok := f.Find("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := f.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)

hemi/internal/relay_tcps.go
@@ -38,7 +38,7 @@ func (f *tcpsRelay) OnShutdown() {
 
 func (f *tcpsRelay) OnConfigure() {
 	// proxyMode
-	if v, ok := f.Prop("proxyMode"); ok {
+	if v, ok := f.Find("proxyMode"); ok {
 		if mode, ok := v.String(); ok && (mode == "socks" || mode == "https" || mode == "reverse") {
 			f.proxyMode = mode
 		} else {
@@ -48,7 +48,7 @@ func (f *tcpsRelay) OnConfigure() {
 		f.proxyMode = "reverse"
 	}
 	// toBackend
-	if v, ok := f.Prop("toBackend"); ok {
+	if v, ok := f.Find("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := f.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)

hemi/internal/relay_udps.go
@@ -37,7 +37,7 @@ func (f *udpsRelay) OnShutdown() {
 
 func (f *udpsRelay) OnConfigure() {
 	// toBackend
-	if v, ok := f.Prop("toBackend"); ok {
+	if v, ok := f.Find("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := f.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)

hemi/internal/rpc.go
@@ -65,7 +65,7 @@ func (s *Svc) OnShutdown() {
 
 func (s *Svc) OnConfigure() {
 	// withStater
-	if v, ok := s.Prop("withStater"); ok {
+	if v, ok := s.Find("withStater"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if stater := s.stage.Stater(name); stater == nil {
 				UseExitf("unknown stater: '%s'\n", name)

hemi/internal/server.go
@@ -52,7 +52,7 @@ func (s *Server_) OnCreate(name string, stage *Stage) { // exported
 
 func (s *Server_) OnConfigure() {
 	// address
-	if v, ok := s.Prop("address"); ok {
+	if v, ok := s.Find("address"); ok {
 		if address, ok := v.String(); ok {
 			if p := strings.IndexByte(address, ':'); p == -1 || p == len(address)-1 {
 				UseExitln("bad address: " + address)

hemi/internal/static.go
@@ -50,7 +50,7 @@ func (h *staticHandlet) OnShutdown() {
 
 func (h *staticHandlet) OnConfigure() {
 	// webRoot
-	if v, ok := h.Prop("webRoot"); ok {
+	if v, ok := h.Find("webRoot"); ok {
 		if dir, ok := v.String(); ok && dir != "" {
 			h.webRoot = dir
 		} else {
@@ -69,7 +69,7 @@ func (h *staticHandlet) OnConfigure() {
 		}
 	}
 	// aliasTo
-	if v, ok := h.Prop("aliasTo"); ok {
+	if v, ok := h.Find("aliasTo"); ok {
 		if fromTo, ok := v.StringListN(2); ok {
 			h.aliasTo = fromTo
 		} else {
@@ -83,7 +83,7 @@ func (h *staticHandlet) OnConfigure() {
 	// autoIndex
 	h.ConfigureBool("autoIndex", &h.autoIndex, false)
 	// mimeTypes
-	if v, ok := h.Prop("mimeTypes"); ok {
+	if v, ok := h.Find("mimeTypes"); ok {
 		if mimeTypes, ok := v.StringDict(); ok {
 			h.mimeTypes = make(map[string]string)
 			for ext, mimeType := range staticDefaultMimeTypes {

hemi/internal/web.go
@@ -2096,7 +2096,7 @@ func (a *App) OnConfigure() {
 	a.contentSaver_.onConfigure(a, TempDir()+"/apps/"+a.name)
 
 	// hostnames
-	if v, ok := a.Prop("hostnames"); ok {
+	if v, ok := a.Find("hostnames"); ok {
 		hostnames, ok := v.StringList()
 		if !ok || len(hostnames) == 0 {
 			UseExitln("empty hostnames")
@@ -2146,7 +2146,7 @@ func (a *App) OnConfigure() {
 	// tlsPrivateKey
 	a.ConfigureString("tlsPrivateKey", &a.tlsPrivateKey, func(value string) bool { return value != "" }, "")
 	// accessLog
-	if v, ok := a.Prop("accessLog"); ok {
+	if v, ok := a.Find("accessLog"); ok {
 		if log, ok := v.StringListN(2); ok {
 			a.accessLog = log
 		} else {
@@ -2178,7 +2178,7 @@ func (a *App) OnConfigure() {
 		}
 	}
 	// withStater
-	if v, ok := a.Prop("withStater"); ok {
+	if v, ok := a.Find("withStater"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if stater := a.stage.Stater(name); stater == nil {
 				UseExitf("unknown stater: '%s'\n", name)
@@ -2552,7 +2552,7 @@ func (r *Rule) OnConfigure() {
 	r.ConfigureBytes("returnText", &r.returnText, nil, nil)
 
 	// handlets
-	if v, ok := r.Prop("handlets"); ok {
+	if v, ok := r.Find("handlets"); ok {
 		if len(r.socklets) > 0 {
 			UseExitln("cannot mix handlets and socklets in a rule")
 		}
@@ -2572,7 +2572,7 @@ func (r *Rule) OnConfigure() {
 		}
 	}
 	// revisers
-	if v, ok := r.Prop("revisers"); ok {
+	if v, ok := r.Find("revisers"); ok {
 		if len(r.revisers) != 0 {
 			UseExitln("specifying revisers is not allowed while there are literal revisers")
 		}
@@ -2589,7 +2589,7 @@ func (r *Rule) OnConfigure() {
 		}
 	}
 	// socklets
-	if v, ok := r.Prop("socklets"); ok {
+	if v, ok := r.Find("socklets"); ok {
 		if len(r.handlets) > 0 {
 			UseExitln("cannot mix socklets and handlets in a rule")
 		}

hemi/internal/web_proxy.go
@@ -37,7 +37,7 @@ func (h *normalProxy_) onCreate(name string, stage *Stage, app *App) {
 
 func (h *normalProxy_) onConfigure() {
 	// proxyMode
-	if v, ok := h.Prop("proxyMode"); ok {
+	if v, ok := h.Find("proxyMode"); ok {
 		if mode, ok := v.String(); ok && (mode == "forward" || mode == "reverse") {
 			h.isForward = mode == "forward"
 		} else {
@@ -45,7 +45,7 @@ func (h *normalProxy_) onConfigure() {
 		}
 	}
 	// toBackend
-	if v, ok := h.Prop("toBackend"); ok {
+	if v, ok := h.Find("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := h.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)
@@ -62,7 +62,7 @@ func (h *normalProxy_) onConfigure() {
 		UseExitln("forward proxy can be bound to default app only")
 	}
 	// withCacher
-	if v, ok := h.Prop("withCacher"); ok {
+	if v, ok := h.Find("withCacher"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if cacher := h.stage.Cacher(name); cacher == nil {
 				UseExitf("unknown cacher: '%s'\n", name)
@@ -114,7 +114,7 @@ func (s *socketProxy_) onCreate(name string, stage *Stage, app *App) {
 
 func (s *socketProxy_) onConfigure() {
 	// proxyMode
-	if v, ok := s.Prop("proxyMode"); ok {
+	if v, ok := s.Find("proxyMode"); ok {
 		if mode, ok := v.String(); ok && (mode == "forward" || mode == "reverse") {
 			s.isForward = mode == "forward"
 		} else {
@@ -122,7 +122,7 @@ func (s *socketProxy_) onConfigure() {
 		}
 	}
 	// toBackend
-	if v, ok := s.Prop("toBackend"); ok {
+	if v, ok := s.Find("toBackend"); ok {
 		if name, ok := v.String(); ok && name != "" {
 			if backend := s.stage.Backend(name); backend == nil {
 				UseExitf("unknown backend: '%s'\n", name)
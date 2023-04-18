28f49cdd284794a36e655c6e846666de34252c80
Zhang Jingcheng <diogin@gmail.com>
2023-04-18 23:59:54 +0800

    examples: add example code to myapp

--- hemi/examples/myapp/main.go
+++ hemi/examples/myapp/main.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	. "github.com/hexinfra/gorox/hemi"
 	"github.com/hexinfra/gorox/hemi/process"
+	"github.com/hexinfra/gorox/hemi/contrib/routers/simple"
 )
 
 const usage = `
@@ -60,4 +62,57 @@ OPTIONS
 
 func main() {
 	process.Main("myapp", usage, 0, "127.0.0.1:9528")
+}
+
+func init() {
+	RegisterHandlet("myHandlet", func(name string, stage *Stage, app *App) Handlet {
+		h := new(myHandlet)
+		h.onCreate(name, stage, app)
+		return h
+	})
+}
+
+// myHandlet
+type myHandlet struct {
+	Handlet_
+	stage *Stage
+	app   *App
+}
+
+func (h *myHandlet) onCreate(name string, stage *Stage, app *App) {
+	h.MakeComp(name)
+	h.stage = stage
+	h.app = app
+
+	r := simple.New()
+
+	r.Link("/foo", h.handleFoo)
+
+	h.SetRouter(h, r)
+}
+func (h *myHandlet) OnShutdown() {
+	h.app.SubDone()
+}
+
+func (h *myHandlet) OnConfigure() {}
+func (h *myHandlet) OnPrepare()   {}
+
+func (h *myHandlet) Handle(req Request, resp Response) (next bool) {
+	h.Dispatch(req, resp, h.notFound)
+	return
+}
+func (h *myHandlet) notFound(req Request, resp Response) {
+	resp.Send("handle not found!")
+}
+
+func (h *myHandlet) handleFoo(req Request, resp Response) { // METHOD /foo
+	resp.Echo(req.H("user-agent"))
+}
+
+func (h *myHandlet) GET_(req Request, resp Response) { // GET /
+	resp.Echo("hello, world! ")
+	resp.Echo("this is an example application.")
+}
+func (h *myHandlet) POST_user_login(req Request, resp Response) { // POST /user/login
+	resp.Send("what are you doing?")
 }

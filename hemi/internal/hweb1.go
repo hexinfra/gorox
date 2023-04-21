a232d67ee3f98222d2a17419d716326b57838480
Zhang Jingcheng <diogin@gmail.com>
2023-04-21 14:37:47 +0800

    prepare hweb/1 protocol specific apis

--- hemi/internal/hweb1.go
+++ hemi/internal/hweb1.go
@@ -9,6 +9,94 @@
 
 package internal
 
+// hweb1In_ is used by hweb1Request and B1Response.
+type hweb1In_ = webIn_
+
+func (r *hweb1In_) growHeadB1() bool {
+	return false
+}
+func (r *hweb1In_) recvHeadersB1() bool {
+	return true
+}
+
+func (r *hweb1In_) readContentB1() (p []byte, err error) {
+	return nil, nil
+}
+func (r *hweb1In_) _readSizedContentB1() (p []byte, err error) {
+	return nil, nil
+}
+func (r *hweb1In_) _readUnsizedContentB1() (p []byte, err error) {
+	return nil, nil
+}
+
+func (r *hweb1In_) recvTrailersB1() bool {
+	return true
+}
+func (r *hweb1In_) growChunkedB1() bool {
+	return false
+}
+
+// hweb1Out_ is used by hweb1Response and B1Request.
+type hweb1Out_ = webOut_
+
+func (r *hweb1Out_) addHeaderB1(name []byte, value []byte) bool {
+	return false
+}
+func (r *hweb1Out_) headerB1(name []byte) (value []byte, ok bool) {
+	return nil, false
+}
+func (r *hweb1Out_) hasHeaderB1(name []byte) bool {
+	return false
+}
+func (r *hweb1Out_) delHeaderB1(name []byte) (deleted bool) {
+	return false
+}
+func (r *hweb1Out_) delHeaderAtB1(o uint8) {
+}
+func (r *hweb1Out_) _addFixedHeaderB1(name []byte, value []byte) { // used by finalizeHeaders
+}
+
+func (r *hweb1Out_) sendChainB1() error {
+	return nil
+}
+
+func (r *hweb1Out_) echoChainB1(chunked bool) error { // TODO: coalesce?
+	return nil
+}
+
+func (r *hweb1Out_) trailerB1(name []byte) (value []byte, ok bool) {
+	return nil, false
+}
+func (r *hweb1Out_) addTrailerB1(name []byte, value []byte) bool {
+	return false
+}
+func (r *hweb1Out_) trailersB1() []byte { return nil }
+
+func (r *hweb1Out_) passBytesB1(p []byte) error { return nil }
+
+func (r *hweb1Out_) finalizeUnsizedB1() error {
+	return nil
+}
+
+func (r *hweb1Out_) writeHeadersB1() error { // used by echo and post
+	return nil
+}
+func (r *hweb1Out_) writeBlockB1(block *Block, chunked bool) error {
+	return nil
+}
+func (r *hweb1Out_) _writeTextB1(block *Block, chunked bool) error { // text
+	return nil
+}
+func (r *hweb1Out_) _writeFileB1(block *Block, chunked bool) error { // file
+	return nil
+}
+func (r *hweb1Out_) writeBytesB1(p []byte) error {
+	return nil
+}
+func (r *hweb1Out_) writeVectorB1() error {
+	return nil
+}
+
 //////////////////////////////////////// HWEB/1 protocol elements.
 
 // request  = headRecord *dataRecord [ tailRecord ]

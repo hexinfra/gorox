08306dc01d0627cf21400f905e0c9a2aac96bcdb
Zhang Jingcheng <diogin@gmail.com>
2023-04-24 22:41:53 +0800

    referer: fix copyright text

--- hemi/contrib/handlets/referer/referer_test.go
+++ hemi/contrib/handlets/referer/referer_test.go
@@ -1,9 +1,10 @@
-// Copyright (c) 2020-2023 FengWei <feng19910104@gmail.com>.
+// Copyright (c) 2020-2023 Feng Wei <feng19910104@gmail.com>.
 // Copyright (c) 2022-2023 HexInfra Co., Ltd.
 // All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE.md file.
 
-// referer tests.
+// Referer tests.
+
 package referer
 
 import (

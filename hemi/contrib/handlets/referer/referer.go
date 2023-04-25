7ebc420053981ffd69c5c36cc95b2d7bab772c59
Feng Wei <feng19910104@gmail.com>
2023-04-25 17:55:34 +0800

    fix bug、add regular pattern

--- hemi/contrib/handlets/referer/referer.go
+++ hemi/contrib/handlets/referer/referer.go
@@ -9,6 +9,7 @@ package referer
 
 import (
 	"bytes"
+	"regexp"
 
 	. "github.com/hexinfra/gorox/hemi/internal"
 )
@@ -59,10 +60,22 @@ func (h *refererChecker) OnConfigure() {
 	h.ConfigureBool("blocked", &h.IsBlocked, false)
 
 }
+
 func (h *refererChecker) OnPrepare() {
 	h.serverNameRules = make([]*refererRule, 0, len(h.serverNames))
 	for _, serverName := range h.serverNames {
+		if len(serverName) == 0 {
+			continue
+		}
+
 		r := &refererRule{}
+		if serverName[0] == '~' {
+			r.matchType = regexpMatch
+			r.pcre = regexp.MustCompile(string(serverName[1:]))
+			h.serverNameRules = append(h.serverNameRules, r)
+			continue
+		}
+
 		pathIndex := bytes.IndexByte(serverName, '/')
 		if pathIndex == -1 {
 			r.path = nil
@@ -75,13 +88,9 @@ func (h *refererChecker) OnPrepare() {
 		if r.hostname[0] == '*' {
 			r.matchType = suffixMatch
 		} else if r.hostname[len(r.hostname)-1] == '*' {
-			if bytes.Count(r.hostname, []byte(".")) == 1 {
-				r.matchType = prefixMatch2
-			} else {
-				r.matchType = prefixMatch
-			}
+			r.matchType = prefixMatch
 		} else {
-			r.matchType = suffixMatch
+			r.matchType = fullMatch
 		}
 		h.serverNameRules = append(h.serverNameRules, r)
 	}
@@ -94,6 +103,7 @@ func (h *refererChecker) Handle(req Request, resp Response) (next bool) {
 	var (
 		hostname, path []byte
 		lastIndex      = -1
+		schemeLen      = 0
 	)
 	refererURL, ok := req.UnsafeHeader("referer")
 	if !ok {
@@ -106,7 +116,7 @@ func (h *refererChecker) Handle(req Request, resp Response) (next bool) {
 		return true
 	}
 
-	hostname, path = getHostNameAndPath(refererURL)
+	hostname, path, schemeLen = getHostNameAndPath(refererURL)
 	if hostname == nil {
 		goto forbidden
 	}
@@ -116,8 +126,13 @@ func (h *refererChecker) Handle(req Request, resp Response) (next bool) {
 		if lastIndex == -1 && rule.matchType != fullMatch {
 			continue
 		}
-		if rule.match(hostname) {
-			if len(rule.path) > 0 && bytes.Equal(rule.path, path) {
+
+		if rule.matchType == regexpMatch {
+			if rule.match(refererURL[schemeLen:]) {
+				return true
+			}
+		} else if rule.match(hostname) {
+			if len(rule.path) > 0 && bytes.HasPrefix(path, rule.path) {
 				return true
 			}
 		}
@@ -131,14 +146,31 @@ forbidden:
 
 func checkRule(rules [][]byte) bool {
 	for _, rule := range rules {
+		if rule[0] == '~' { // regular expression
+			if _, err := regexp.Compile(string(rule[1:])); err != nil {
+				return false
+			}
+			continue
+		}
+
+		// start with http[s]:// is not allowed
+		if bytes.HasPrefix(rule, httpScheme) || bytes.HasPrefix(rule, httpsScheme) {
+			return false
+		}
+		// not allow multiple '*', except for regular expressions.
+		if bytes.Count(rule, []byte("*")) > 1 {
+			return false
+		}
+
 		pathIndex := bytes.IndexByte(rule, '/')
 		if pathIndex == -1 {
 			pathIndex = len(rules)
 		}
 
+		// '*' not allowed in the middle
 		idx := bytes.IndexByte(rule[:pathIndex], '*')
 		if idx != -1 && (idx > 0 && idx < len(rule[:pathIndex-1])) {
-			return false // '*' not allowed in the middle
+			return false
 		}
 		if bytes.HasPrefix(rule, httpScheme) || bytes.HasPrefix(rule, httpsScheme) {
 			return false
@@ -150,7 +182,7 @@ func checkRule(rules [][]byte) bool {
 	return true
 }
 
-func getHostNameAndPath(refererURL []byte) (hostname, path []byte) {
+func getHostNameAndPath(refererURL []byte) (hostname, path []byte, schemeLen int) {
 	cb := func(scheme, url []byte) bool {
 		if len(url) < len(scheme) {
 			return false
@@ -159,7 +191,8 @@ func getHostNameAndPath(refererURL []byte) (hostname, path []byte) {
 			return false
 		}
 
-		url = url[len(scheme):]
+		schemeLen = len(scheme)
+		url = url[schemeLen:]
 		queryIndex := bytes.IndexByte(url, '?')
 		if queryIndex == -1 {
 			queryIndex = len(url)
@@ -190,13 +223,14 @@ const (
 	fullMatch = iota
 	suffixMatch
 	prefixMatch
-	prefixMatch2
+	regexpMatch
 )
 
 type refererRule struct {
 	matchType int
 	hostname  []byte
 	path      []byte
+	pcre      *regexp.Regexp
 }
 
 func (r *refererRule) match(hostname []byte) bool {
@@ -207,8 +241,8 @@ func (r *refererRule) match(hostname []byte) bool {
 		return r.suffixMatch(hostname)
 	case prefixMatch:
 		return r.prefixMatch(hostname)
-	case prefixMatch2:
-		return r.prefixMatch2(hostname)
+	case regexpMatch:
+		return r.regexpMatch(hostname)
 	}
 
 	return false
@@ -229,11 +263,7 @@ func (r *refererRule) prefixMatch(hostname []byte) bool {
 	return bytes.HasPrefix(hostname, r.hostname[:len(r.hostname)-1])
 }
 
-// for example:  bar.*
-func (r *refererRule) prefixMatch2(hostname []byte) bool {
-	lastIndex := bytes.LastIndexByte(hostname, '.')
-	if lastIndex == -1 {
-		return false
-	}
-	return bytes.HasPrefix(hostname[:lastIndex], r.hostname[:len(r.hostname)-1])
+// for example:  ~\.bar\.
+func (r *refererRule) regexpMatch(hostname []byte) bool {
+	return r.pcre.Match(hostname)
 }
--- hemi/contrib/handlets/referer/referer_test.go
+++ hemi/contrib/handlets/referer/referer_test.go
@@ -9,6 +9,8 @@ package referer
 
 import (
 	"bytes"
+	"reflect"
+	"regexp"
 	"testing"
 )
 
@@ -17,28 +19,29 @@ func TestGetHostNameAndPath(t *testing.T) {
 		input   string
 		expect1 []byte
 		expect2 []byte
+		expect3 int
 	}{
-		{"http://www.a.org/a/b/c", []byte("www.a.org"), []byte("/a/b/c")},
-		{"https://www.a.org/a/b/c", []byte("www.a.org"), []byte("/a/b/c")},
-		{"https://www.a.org:8080/a/b/c", []byte("www.a.org"), []byte("/a/b/c")},
-		{"www.a.org/a/b/c", []byte("www.a.org"), []byte("/a/b/c")},
-		{"www.a.org:8080/a?a=1", []byte("www.a.org"), []byte("/a")},
-		{"http://www.a.org?a=1", []byte("www.a.org"), nil},
-		{"http://a.io?a=1", []byte("a.io"), nil},
-		{"http://localhost?a=1", []byte("localhost"), nil},
-		{"http://localhost:8080/", []byte("localhost"), []byte("/")},
-		{"http:///", nil, []byte("/")},
-		{"https://", nil, nil},
-		{"http://", nil, nil},
-		{"a.io:8080?a=1", []byte("a.io"), nil},
-		{"a.io?a=1", []byte("a.io"), nil},
-		{"a.io", []byte("a.io"), nil},
-		{"localhost:8080", []byte("localhost"), nil},
-		{"https///", []byte("https"), []byte("///")},
+		{"http://www.gorox.org/a/b/c", []byte("www.gorox.org"), []byte("/a/b/c"), 7},
+		{"https://www.gorox.org/a/b/c", []byte("www.gorox.org"), []byte("/a/b/c"), 8},
+		{"https://www.gorox.org:8080/a/b/c", []byte("www.gorox.org"), []byte("/a/b/c"), 8},
+		{"www.gorox.org/a/b/c", []byte("www.gorox.org"), []byte("/a/b/c"), 0},
+		{"www.gorox.org:8080/a?a=1", []byte("www.gorox.org"), []byte("/a"), 0},
+		{"http://www.gorox.org?a=1", []byte("www.gorox.org"), nil, 7},
+		{"http://gorox.io?a=1", []byte("gorox.io"), nil, 7},
+		{"http://localhost?a=1", []byte("localhost"), nil, 7},
+		{"http://localhost:8080/", []byte("localhost"), []byte("/"), 7},
+		{"http:///", nil, []byte("/"), 7},
+		{"https://", nil, nil, 8},
+		{"http://", nil, nil, 7},
+		{"gorox.io:8080?a=1", []byte("gorox.io"), nil, 0},
+		{"gorox.io?a=1", []byte("gorox.io"), nil, 0},
+		{"gorox.io", []byte("gorox.io"), nil, 0},
+		{"localhost:8080", []byte("localhost"), nil, 0},
+		{"https///", []byte("https"), []byte("///"), 0},
 	}
 
 	for idx, test := range tests {
-		recv1, recv2 := getHostNameAndPath([]byte(test.input))
+		recv1, recv2, recv3 := getHostNameAndPath([]byte(test.input))
 		if !bytes.Equal(recv1, test.expect1) {
 			t.Errorf("#%d: recv1=%s, expect1=%s", idx, recv1, test.expect1)
 		}
@@ -46,11 +49,133 @@ func TestGetHostNameAndPath(t *testing.T) {
 		if !bytes.Equal(recv2, test.expect2) {
 			t.Errorf("#%d: recv2=%s, expect2=%s", idx, recv2, test.expect2)
 		}
+
+		if recv3 != test.expect3 {
+			t.Errorf("#%d: recv3=%v, expect3=%v", idx, recv3, test.expect3)
+		}
+	}
+}
+
+func TestCheckRule(t *testing.T) {
+	tests := []struct {
+		input  string
+		expect bool
+	}{
+		{"*.gorox.com", true},
+		{"*.gorox.*", false},
+		{"gorox.com", true},
+		{"www.gorox.*", true},
+		{"gorox.*", true},
+		{"gorox.*/app", true},
+		{"www.gorox.com/app", true},
+		{"www.gorox.com:8080/app", true},
+		{"http://www.gorox.com:8080/app", false},
+		{`~\.gorox\.`, true},
+		{`~.*`, true},
+		{`~.*/app`, true},
+		{`~(?-i)gorox.net`, true},
+		{`~]\`, false},
+	}
+
+	for idx, test := range tests {
+		recv := checkRule([][]byte{[]byte(test.input)})
+		if recv != test.expect {
+			t.Errorf("#%d: recv=%v, expect=%v", idx, recv, test.expect)
+		}
+	}
+}
+
+func TestOnPrepare(t *testing.T) {
+	tests := []struct {
+		input    []byte
+		expected *refererRule
+	}{
+		{
+			[]byte("*.gorox.com"),
+			&refererRule{suffixMatch, []byte("*.gorox.com"), nil, nil},
+		},
+		{
+			[]byte("*.gorox.com/app"),
+			&refererRule{suffixMatch, []byte("*.gorox.com"), []byte("/app"), nil},
+		},
+
+		{
+			[]byte("gorox.*"),
+			&refererRule{prefixMatch, []byte("gorox.*"), nil, nil}},
+		{
+			[]byte("www.gorox.com"),
+			&refererRule{fullMatch, []byte("www.gorox.com"), nil, nil},
+		},
+		{
+			[]byte(`~\.gorox\.`),
+			&refererRule{regexpMatch, nil, nil, regexp.MustCompile(`\.gorox\.`)},
+		},
+		{
+			[]byte(`~.*`),
+			&refererRule{regexpMatch, nil, nil, regexp.MustCompile(`.*`)},
+		},
+	}
+
+	for idx, test := range tests {
+		r := &refererChecker{}
+		r.serverNames = [][]byte{test.input}
+		r.OnPrepare()
+		if test.expected == nil && len(r.serverNameRules) == 0 {
+			continue
+		}
+
+		if !reflect.DeepEqual(test.expected, r.serverNameRules[0]) {
+			t.Errorf("#%d: recv=%v, expect=%v", idx, r.serverNameRules[0], test.expected)
+		}
 	}
 }
 
 func TestHandle(t *testing.T) {
+	/*
+	   	tests := []struct {
+	   		referer     string
+	   		serverNames []byte
+	   		noneReferer bool
+	   		isBlocked   bool
+	   		expected    bool
+	   	}{
 
-	// TODO: mockRequest
-	// TODO: mockResponse
+	   		{"http://www.example.org", []byte("www.example.org"), false, false, true},
+	   		{"http://www.example.org", nil, true, false, true},
+	   		{"http://", nil, false, true, true},
+	   		{"", nil, false, true, false},
+	   		{"", nil, true, false, true},
+	   		{"xxxxxx", []byte("*.gorox.org"), false, true, true},
+	   		{"http://localhost", []byte("localhost"), false, false, true},
+	   		{"http://www.gorox.org", []byte("*.gorox.org"), false, false, true},
+	   		{"http://www.gorox.org", []byte("gorox.org"), false, false, false},
+	   		{"http://www.gorox.org", []byte("gorox.*"), false, false, false},
+	   		{"http://gorox.org", []byte("gorox.*"), false, false, true},
+	   		{"http://gorox.com", []byte("gorox.*"), false, false, true},
+	   		{"http://gorox.com/a/p/p", []byte("gorox.*\/a/p/p"), false, false, true},
+	   		{"http://gorox.com/a/p/p", []byte("gorox.*\/a/"), false, false, true},
+	   		{"http://gorox.com:8080/a/p/p", []byte("gorox.*"), false, false, true},
+	   		{"http://bar.com:8080/a/p/p", []byte(`~bar.org`), false, false, true},
+	   		{"http://h5.bar.com:8080/a/p/p", []byte(`~\.bar\.`), false, false, true},
+	   		{"http://h5.bar.com:8080/a/p/p", []byte(`(?-i).bar.com`), false, false, true},
+	   		{"http://www.gorox.org/uri", []byte(`~gorox.org/uri`), false, false, true},
+	   		{"http://www.gorox.org", []byte(`~gorox.org/uri`), false, false, true},
+	   		{"http://www.gorox.org/uRI", []byte(`~gorox.org/uri`), false, false, false},
+	   		{"http://www.gorox.org/uRI", []byte(`~example.org$`), false, false, false},
+	   		{"http://book.h5.gorox.org", []byte(`~example.org$`), false, false, false},
+	   	}
+
+	   	for idx, test := range tests {
+	   		if 2 < 1 {
+	   			t.Errorf("#%d: recv=%v, expect=%v", idx, "a", test.expected)
+	   		}
+
+	   }
+
+	   req := MockHttp1Request{}
+	   referer := "http://www.example.org"
+	   req.WriteHeader([]byte(`GET / HTTP/1.1\r\nhost: teest\r\nreferer: ` + referer + `\r\n\r\n`))
+	   r, _ := req.Header("referer")
+	   t.Errorf("#%d: recv=%v, expect=%v", 1, req.AllHeaders(), r)
+	*/
 }

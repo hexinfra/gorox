cb55bdb0d8aaba8f3e786d7a616544943609c359
Zhang Jingcheng <diogin@gmail.com>
2023-05-10 10:56:08 +0800

    fix defaults.def

--- hemi/internal/component.go
+++ hemi/internal/component.go
@@ -358,6 +358,7 @@ type Stage struct {
 	http1Outgate *HTTP1Outgate         // for fast accessing
 	http2Outgate *HTTP2Outgate         // for fast accessing
 	http3Outgate *HTTP3Outgate         // for fast accessing
+	hweb1Outgate *HWEB1Outgate         // for fast accessing
 	hweb2Outgate *HWEB2Outgate         // for fast accessing
 	quicOutgate  *QUICOutgate          // for fast accessing
 	tcpsOutgate  *TCPSOutgate          // for fast accessing
@@ -395,6 +396,7 @@ func (s *Stage) onCreate() {
 	s.http1Outgate = createHTTP1Outgate(s)
 	s.http2Outgate = createHTTP2Outgate(s)
 	s.http3Outgate = createHTTP3Outgate(s)
+	s.hweb1Outgate = createHWEB1Outgate(s)
 	s.hweb2Outgate = createHWEB2Outgate(s)
 	s.quicOutgate = createQUICOutgate(s)
 	s.tcpsOutgate = createTCPSOutgate(s)
@@ -407,6 +409,7 @@ func (s *Stage) onCreate() {
 	s.fixtures[signHTTP1Outgate] = s.http1Outgate
 	s.fixtures[signHTTP2Outgate] = s.http2Outgate
 	s.fixtures[signHTTP3Outgate] = s.http3Outgate
+	s.fixtures[signHWEB1Outgate] = s.hweb1Outgate
 	s.fixtures[signHWEB2Outgate] = s.hweb2Outgate
 	s.fixtures[signQUICOutgate] = s.quicOutgate
 	s.fixtures[signTCPSOutgate] = s.tcpsOutgate
@@ -473,6 +476,7 @@ func (s *Stage) OnShutdown() {
 	go s.http1Outgate.OnShutdown() // we don't treat this as goroutine
 	go s.http2Outgate.OnShutdown() // we don't treat this as goroutine
 	go s.http3Outgate.OnShutdown() // we don't treat this as goroutine
+	go s.hweb1Outgate.OnShutdown() // we don't treat this as goroutine
 	go s.hweb2Outgate.OnShutdown() // we don't treat this as goroutine
 	go s.quicOutgate.OnShutdown()  // we don't treat this as goroutine
 	go s.tcpsOutgate.OnShutdown()  // we don't treat this as goroutine
@@ -508,6 +512,7 @@ func (s *Stage) OnConfigure() {
 	}, LogsDir()+"/worker.log")
 
 	tempDir := TempDir()
+
 	// cpuFile
 	s.ConfigureString("cpuFile", &s.cpuFile, func(value string) error {
 		if value == "" {
@@ -591,7 +596,7 @@ func (s *Stage) OnPrepare() {
 
 func (s *Stage) createRunner(sign string, name string) Runner {
 	if s.Runner(name) != nil {
-		UseExitf("conflicting runner with a same sign '%s'\n", name)
+		UseExitf("conflicting runner with a same name '%s'\n", name)
 	}
 	create, ok := runnerCreators[sign]
 	if !ok {
@@ -706,7 +711,7 @@ func (s *Stage) createServer(sign string, name string) Server {
 }
 func (s *Stage) createCronjob(sign string, name string) Cronjob {
 	if s.Cronjob(name) != nil {
-		UseExitf("conflicting cronjob with a same sign '%s'\n", name)
+		UseExitf("conflicting cronjob with a same name '%s'\n", name)
 	}
 	create, ok := cronjobCreators[sign]
 	if !ok {
@@ -724,6 +729,7 @@ func (s *Stage) Resolver() *resolverFixture  { return s.resolver }
 func (s *Stage) HTTP1Outgate() *HTTP1Outgate { return s.http1Outgate }
 func (s *Stage) HTTP2Outgate() *HTTP2Outgate { return s.http2Outgate }
 func (s *Stage) HTTP3Outgate() *HTTP3Outgate { return s.http3Outgate }
+func (s *Stage) HWEB1Outgate() *HWEB1Outgate { return s.hweb1Outgate }
 func (s *Stage) HWEB2Outgate() *HWEB2Outgate { return s.hweb2Outgate }
 func (s *Stage) QUICOutgate() *QUICOutgate   { return s.quicOutgate }
 func (s *Stage) TCPSOutgate() *TCPSOutgate   { return s.tcpsOutgate }
@@ -1017,6 +1023,8 @@ func (s *Stage) ProfBlock() {
 // Fixtures only exist in internal, and are created by stage.
 // Some critical functions, like clock and name resolver, are
 // implemented as fixtures.
+//
+// Fixtures are singletons in stage.
 type fixture interface {
 	Component
 	run() // goroutine

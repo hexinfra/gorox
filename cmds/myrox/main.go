9929b55bc76378e744dd9613d7cb823f80b1bfd7
Zhang Jingcheng <diogin@gmail.com>
2023-05-10 18:58:57 +0800

    compact help messages

--- cmds/myrox/main.go
+++ cmds/myrox/main.go
@@ -25,25 +25,25 @@ Myrox (%s)
 ACTION
 ------
 
-  serve        # start as server
-  check        # dry run to check config
-  help         # show this message
-  version      # show version info
-  advise       # show how to optimize current platform
-  stop         # tell server to exit immediately
-  quit         # tell server to exit gracefully
-  pid          # call server to report pids of leader and worker
-  leader       # call leader to report its info
-  rework       # tell leader to restart worker gracefully
-  readmin      # tell leader to reopen its admin interface
-  ping         # call leader to give a pong
-  worker       # call worker to report its info
-  reload       # tell worker to reload config
-  cpu          # tell worker to perform cpu profiling
-  heap         # tell worker to perform heap profiling
-  thread       # tell worker to perform thread profiling
-  goroutine    # tell worker to perform goroutine profiling
-  block        # tell worker to perform block profiling
+  serve      # start as server
+  check      # dry run to check config
+  help       # show this message
+  version    # show version info
+  advise     # show how to optimize current platform
+  stop       # tell server to exit immediately
+  quit       # tell server to exit gracefully
+  pid        # call server to report pids of leader and worker
+  leader     # call leader to report its info
+  rework     # tell leader to restart worker gracefully
+  readmin    # tell leader to reopen its admin interface
+  ping       # call leader to give a pong
+  worker     # call worker to report its info
+  reload     # tell worker to reload config
+  cpu        # tell worker to perform cpu profiling
+  heap       # tell worker to perform heap profiling
+  thread     # tell worker to perform thread profiling
+  goroutine  # tell worker to perform goroutine profiling
+  block      # tell worker to perform block profiling
 
   Only one action is allowed at a time.
   If ACTION is not specified, the default action is "serve".
@@ -51,21 +51,21 @@ ACTION
 OPTIONS
 -------
 
-  -debug  <level>     # debug level (default: 0, means disable. max: 2)
-  -target <addr>      # leader address to tell or call (default: 127.0.0.1:9528)
-  -admin  <addr>      # listen address of leader admin (default: 127.0.0.1:9528)
-  -conf   <config>    # path or url to worker config file
-  -single             # run server in single mode. only a process is started
-  -daemon             # run server as daemon (default: false)
-  -log    <path>      # leader log file (default: myrox-leader.log in logs dir)
-  -base   <path>      # base directory of the program
-  -logs   <path>      # logs directory to use
-  -temp   <path>      # temp directory to use
-  -vars   <path>      # vars directory to use
+  -debug  <level>   # debug level (default: 0, means disable. max: 2)
+  -target <addr>    # leader address to tell or call (default: 127.0.0.1:9528)
+  -admin  <addr>    # listen address of leader admin (default: 127.0.0.1:9528)
+  -conf   <config>  # path or url to worker config file
+  -single           # run server in single mode. only a process is started
+  -daemon           # run server as daemon (default: false)
+  -log    <path>    # leader log file (default: myrox-leader.log in logs dir)
+  -base   <path>    # base directory of the program
+  -logs   <path>    # logs directory to use
+  -temp   <path>    # temp directory to use
+  -vars   <path>    # vars directory to use
 
   "-debug" applies for all actions.
   "-target" applies for telling and calling actions only.
-  "-admin" applies for "serve" and "readmin".
+  "-admin" and "-myrox" apply for "serve" and "readmin".
   Other options apply for "serve" only.
 
 `
--- hemi/gosites/main.go
+++ hemi/gosites/main.go
@@ -20,25 +20,25 @@ Gosites (%s)
 ACTION
 ------
 
-  serve        # start as server
-  check        # dry run to check config
-  help         # show this message
-  version      # show version info
-  advise       # show how to optimize current platform
-  stop         # tell server to exit immediately
-  quit         # tell server to exit gracefully
-  pid          # call server to report pids of leader and worker
-  leader       # call leader to report its info
-  rework       # tell leader to restart worker gracefully
-  readmin      # tell leader to reopen its admin interface
-  ping         # call leader to give a pong
-  worker       # call worker to report its info
-  reload       # tell worker to reload config
-  cpu          # tell worker to perform cpu profiling
-  heap         # tell worker to perform heap profiling
-  thread       # tell worker to perform thread profiling
-  goroutine    # tell worker to perform goroutine profiling
-  block        # tell worker to perform block profiling
+  serve      # start as server
+  check      # dry run to check config
+  help       # show this message
+  version    # show version info
+  advise     # show how to optimize current platform
+  stop       # tell server to exit immediately
+  quit       # tell server to exit gracefully
+  pid        # call server to report pids of leader and worker
+  leader     # call leader to report its info
+  rework     # tell leader to restart worker gracefully
+  readmin    # tell leader to reopen its admin interface
+  ping       # call leader to give a pong
+  worker     # call worker to report its info
+  reload     # tell worker to reload config
+  cpu        # tell worker to perform cpu profiling
+  heap       # tell worker to perform heap profiling
+  thread     # tell worker to perform thread profiling
+  goroutine  # tell worker to perform goroutine profiling
+  block      # tell worker to perform block profiling
 
   Only one action is allowed at a time.
   If ACTION is not specified, the default action is "serve".
@@ -46,20 +46,20 @@ ACTION
 OPTIONS
 -------
 
-  -target <addr>      # leader address to tell or call (default: 127.0.0.1:9525)
-  -admin  <addr>      # listen address of leader admin (default: 127.0.0.1:9525)
-  -myrox  <addr>      # myrox address to join. if set, "-admin" will be ignored
-  -conf   <config>    # path or url to worker config file
-  -single             # run server in single mode. only a process is started
-  -daemon             # run server as daemon (default: false)
-  -log    <path>      # leader log file (default: gosites-leader.log in logs dir)
-  -base   <path>      # base directory of the program
-  -logs   <path>      # logs directory to use
-  -temp   <path>      # temp directory to use
-  -vars   <path>      # vars directory to use
+  -target <addr>    # leader address to tell or call (default: 127.0.0.1:9525)
+  -admin  <addr>    # listen address of leader admin (default: 127.0.0.1:9525)
+  -myrox  <addr>    # myrox address to join. if set, "-admin" will be ignored
+  -conf   <config>  # path or url to worker config file
+  -single           # run server in single mode. only a process is started
+  -daemon           # run server as daemon (default: false)
+  -log    <path>    # leader log file (default: gosites-leader.log in logs dir)
+  -base   <path>    # base directory of the program
+  -logs   <path>    # logs directory to use
+  -temp   <path>    # temp directory to use
+  -vars   <path>    # vars directory to use
 
   "-target" applies for telling and calling actions only.
-  "-admin" applies for "serve" and "readmin".
+  "-admin" and "-myrox" apply for "serve" and "readmin".
   Other options apply for "serve" only.
 
 `

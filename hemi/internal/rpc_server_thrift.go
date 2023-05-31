6d96c46e58633613c4fed9f2d0ae50fa227bfc9d
Zhang Jingcheng <diogin@gmail.com>
2023-05-31 13:36:41 +0800

    internal: rename more files

---
+++ hemi/internal/rpc_server_thrift.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2020-2023 Zhang Jingcheng <diogin@gmail.com>.
+// Copyright (c) 2022-2023 HexInfra Co., Ltd.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE.md file.
+
+// Thrift server bridge.
+
+package internal
+
+// ThriftBridge is the interface for all Thrift server bridges.
+// Users can implement their own Thrift server in exts, which may embeds thrift.TServer and must implements the ThriftBridge interface.
+type ThriftBridge interface {
+	rpcServer
+
+	ThriftServer() any // may be a thrift.TServer?
+}
